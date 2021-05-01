cbfb03d5119d17c05300547f491cb680edda11c2
SeonJungKim <ksj083895@gmail.com>
2021-05-01 22:07:20 +0500

    Initial Commit

---
+++ SwiftUI_Rest/SwiftUI_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RestApp.swift
+//  SwiftUI_Rest
+//
+//  Created by 김선중 on 2021/05/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
