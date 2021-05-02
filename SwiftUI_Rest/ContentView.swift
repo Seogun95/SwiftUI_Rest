10a8c11ffa40744fce7b771ee03dfb9f7cda0c5e
SeonJungKim <ksj083895@gmail.com>
2021-05-02 17:31:00 +0500

    정리

--- SwiftUI_Rest/ContentView.swift
+++ SwiftUI_Rest/ContentView.swift
@@ -12,44 +12,45 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                Text("당신은 언제 기상하십니까?")
-                    .font(.headline)
-                    .padding(.top, 50)
-                
-                DatePicker("시간을 입력해주세요.", selection: $wakeup, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                
-                Spacer()
-                
-                Text("수면 시간")
-                    .font(.headline)
-                
-                Stepper(value: $sleepAmount, in: 3...12, step: 0.5) {
-                    Text("\(sleepAmount, specifier: "%g") 시간")
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("당신은 언제 기상하십니까?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a date", selection: $wakeup,
+                               displayedComponents: .hourAndMinute)
+                        .datePickerStyle(WheelDatePickerStyle())
+                        .labelsHidden()
                 }
                 
-                Spacer()
-                
-                Text("하루에 마시는 커피 양")
-                    .font(.headline)
-                Stepper(value:$coffeeAmount, in: 1...15) {
-                    if coffeeAmount == 1 {
-                        Text("1 잔")
-                    } else {
-                        Text("\(coffeeAmount) 잔")
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("수면 시간")
+                        .font(.headline)
+                    
+                    Stepper(value: $sleepAmount, in: 3...12, step: 0.5) {
+                        Text("\(sleepAmount, specifier: "%g") 시간")
                     }
                     
                 }
-                Spacer().frame(height: 90)
                 
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("하루에 마시는 커피 양")
+                        .font(.headline)
+                    Stepper(value:$coffeeAmount, in: 1...15) {
+                        if coffeeAmount == 1 {
+                            Text("1 잔")
+                        } else {
+                            Text("\(coffeeAmount) 잔")
+                        }
+                        
+                    }
+                    
+                }
             }
             
-            .padding()
-            .frame(height: 500)
             .navigationBarTitle("충분한 휴식")
             .navigationBarItems(trailing:
                                     Button(action: calculateBedtime) {
@@ -62,7 +63,7 @@ struct ContentView: View {
     }
     
     func calculateBedtime() {
-        
+        let model = SleepCalculator()
     }
 }
 
